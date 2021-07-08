214466cfca2788415bba11afc65267025ddb0723
ytppp <651003708@qq.com>
2021-07-08 23:48:10 +0800

    feat: add jira-dev-tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ProjectList from "screens/project-list";
-import TsReactTest from "screens/ts-react-tsst";
+import Login from "screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
-      <TsReactTest />
+      <Login />
     </div>
   );
 }
